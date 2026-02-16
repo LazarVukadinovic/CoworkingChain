154ceb1bbd71a4a533f6f78e581a100d96f4a124
Lazar <lazarvukadinovic7@gmail.com>
2026-02-16 16:10:00 +0100

    Start form created

---
+++ src/Coworking.WinForms/Start.cs
@@ -0,0 +1,23 @@
+namespace Coworking.WinForms
+{
+    public partial class Start : Form
+    {
+        public Start()
+        {
+            InitializeComponent();
+            comboBox_configFile.Items.Add("config1.txt");
+            comboBox_configFile.Items.Add("config2.txt");
+
+        }
+
+        private void pokreni_btn_Click(object sender, EventArgs e)
+        {
+            string configFileName = comboBox_configFile.Text;
+            var configFilePath = Path.Combine(AppContext.BaseDirectory, configFileName);
+
+            // TODO: pozivanje DB modela za povezivanje na konekcioni string
+
+
+        }
+    }
+}
