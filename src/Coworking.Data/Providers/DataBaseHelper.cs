4e2971049b61dc59d76ae2ade063dd9d7239305c
Vanja <vanjarado@gmail.com>
2026-02-17 14:50:52 +0100

    Added DataBaseHelper

---
+++ src/Coworking.Data/Providers/DataBaseHelper.cs
@@ -0,0 +1,26 @@
+﻿using Coworking.Data.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coworking.Data.Providers
+{
+    internal class DataBaseHelper
+    {
+        public IDataBaseFactory? vratiFactory(string konekcioniString)
+        {
+            konekcioniString = konekcioniString.ToLower();
+            if (konekcioniString.Contains("user id") && konekcioniString.Contains("password"))
+            {
+                return new MySqlFactory();
+            }
+            else if (konekcioniString.Contains("trusted_connection") || konekcioniString.Contains("trustservercertificate"))
+            {
+                return new MsSqlFactory();
+            }
+            else return null;
+        }
+    }
+}
