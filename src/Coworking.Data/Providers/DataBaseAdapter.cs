782ede6d569784834c71ae96d73ca640e0b831d1
Vanja <vanjarado@gmail.com>
2026-02-17 13:13:10 +0100

    Added DataBaseAdapter

---
+++ src/Coworking.Data/Providers/DataBaseAdapter.cs
@@ -0,0 +1,45 @@
+﻿using Coworking.Data.Factories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coworking.Data.Providers
+{
+    internal class DataBaseAdapter
+    {
+        private readonly IDataBaseFactory _factory;
+        private readonly string konekcioniString;
+
+        public DataBaseAdapter(IDataBaseFactory factory, string konekcioniString)
+        {
+            _factory = factory;
+            this.konekcioniString = konekcioniString;
+        }
+
+        public DataTable izvrsiUpit(string upit)
+        {
+            using (var connStr = _factory.napraviKonekciju(konekcioniString))
+            using (var cmn=_factory.napraviKomandu(upit,connStr))
+            {
+                var DataSet = new DataSet();
+                connStr.Open();
+                var adapter = _factory.napraviAdapter(cmn);
+                adapter.Fill(DataSet);
+                return DataSet.Tables[0];
+            }
+        }
+
+        public void izvrsiUpitBezRezultata(string upit)
+        {
+            using (var connStr = _factory.napraviKonekciju(konekcioniString))
+            using (var cmn = _factory.napraviKomandu(upit, connStr))
+            {
+                connStr.Open();
+                cmn.ExecuteNonQuery();
+            }
+        }
+    }
+}
