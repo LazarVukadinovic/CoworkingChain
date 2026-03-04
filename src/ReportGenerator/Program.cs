407a214ca563d2800ac69ce7a216332edb4bd9c1
Djordje Janic <djordjejanici5@gmail.com>
2026-03-04 20:52:20 +0100

    Modified ReportGenerator

--- src/ReportGenerator/Program.cs
+++ src/ReportGenerator/Program.cs
@@ -1,14 +1,23 @@
 ﻿using Coworking.Data.Reports;
 using Coworking.Data.Providers;
+using System.Diagnostics;
 
-static void Main(string[] args)
+class Program
 {
-    PeriodStrategy strategy = new PeriodStrategy(PeriodType.Week1);
+    static void Main(string[] args)
+    {
+        PeriodStrategy strategy = new PeriodStrategy(PeriodType.Week1);
 
-    var proxy = DataBaseSingleton.vratiInstancu();
-    var report = new CreateReport(proxy);
-    var rows = report.GenerateReport(strategy);
+        var proxy = DataBaseSingleton.vratiInstancu();
+        var report = new CreateReport(proxy);
+        var rows = report.GenerateReport(strategy);
 
-    var exporter = new CsvExporter();
-    //exporter.ExportToCsv(rows,$"C:\\Reports\\report_{DateTime.Now:yyyy_MM}.csv");
+        var exporter = new CsvExporter();
+
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        string fileName = $"report_{DateTime.Now:yyyy_MM}.csv";
+        string fullPath = Path.Combine(desktopPath, fileName);
+
+        exporter.ExportToCsv(rows, fullPath);
+    }
 }
