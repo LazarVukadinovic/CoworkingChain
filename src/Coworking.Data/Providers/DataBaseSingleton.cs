02e7c100c98725f1c3ee612574264741c9dff788
Lazar <lazolakivukadinovic1001@gmail.com>
2026-03-05 17:58:53 +0100

    DataBaseSingleton dependency creation is transfered to DataBaseDependencyProvider

--- src/Coworking.Data/Providers/DataBaseSingleton.cs
+++ src/Coworking.Data/Providers/DataBaseSingleton.cs
@@ -3,12 +3,8 @@ namespace Coworking.Data.Providers
 {
     public sealed class DataBaseSingleton
     {
-        private static readonly Lazy<IDataBase> _instance = new(() =>
-        {
-            var settings = new DBSettings();
-            var facade = new DataBaseFacade(settings);
-            return new DataBaseProxy(facade);
-        }, isThreadSafe: true);
+        private static readonly Lazy<IDataBase> _instance = 
+            new(() => DataBaseDependencyProvider.Create(), isThreadSafe: true);
 
         private DataBaseSingleton() { }
         public static IDataBase vratiInstancu() => _instance.Value;
